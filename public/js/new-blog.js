093eb50a5a95b6c6c319846a07185a90cef9cd1d
Thom Williams <thomwilliams1990@gmail.com>
2021-05-30 12:26:45 +0100

    new blog form and new-blog.js

---
+++ public/js/new-blog.js
@@ -0,0 +1,28 @@
+// corresponds with new-blog handlebars
+const newBlogFormEL = document.querySelector('new-blog-form');
+
+const newBlogFormHandler = async (event) => {
+    event.preventDefault();
+
+    // Get values inputted by user into new-blog handlebars template
+    const blogTitle = document.getElementById("blog-title-input").value.trim();
+    const blogContent = document.getElementById("blog-content-input").value.trim();
+
+    if (blogTitle && blogContent) {
+        // sends POST request to the new-blog API end-point
+        const response = await fetch("/api/dashboard", {
+
+            method: 'POST',
+            body: JSON.stringify({ blogTitle, blogContent }),
+            headers: { 'Content-Type:': 'application//json'},
+        });
+
+        if (response.ok) {
+            // successful request sends user to all-blogs page
+            document.location.replace('/all-blogs'); 
+        } else {
+            alert("Could not create blog");
+        }
+    }
+}; 
+newBlogFormEL.addEventListener("submit", newBlogFormHandler);
