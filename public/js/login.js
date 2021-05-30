f5292f6bffd1755d7a17dee499546e461945d29c
Thom Williams <thomwilliams1990@gmail.com>
2021-05-30 10:38:55 +0100

    login.js added

---
+++ public/js/login.js
@@ -0,0 +1,30 @@
+// corresponds with login handlebars
+const loginFormEL = document.querySelector('login-form');
+
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    // Get values inputted by user into login handlebars template
+    const username = document.getElementById("username-login").value.trim();
+    const password = document.getElementById("password-login").value.trim();
+
+    if (username && password) {
+        // sends POST request to the login API end-point
+        const response = await fetch("/api/users/login", {
+
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type:': 'application//json'},
+        });
+
+        if (response.ok) {
+            // successful request sends user to all-blogs page
+            document.location.replace('/all-blogs'); 
+        } else {
+            alert(response.statusText);
+        }
+    }
+}; 
+
+
+loginFormEL.addEventListener("submit");
