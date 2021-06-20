7dd6ce781a8880226dca86477748e48caaf8d713
Thom Williams <thomwilliams1990@gmail.com>
2021-06-20 21:46:09 +0100

    styling updated

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,18 +1,18 @@
 const sequelize = require("../config/connection");
-const { User, Blog, Comments } = require("../models");
+const { User, Blog } = require("../models");
 
 const userData = require("./userData.json");
 const blogData = require("./blogData.json");
-const commentsData = require("./commentsData.json");
+// const commentsData = require("./commentsData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+  // await Comments.bulkCreate(commentsData);
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
   await Blog.bulkCreate(blogData);
-  await Comments.bulkCreate(commentsData);
   process.exit(0);
 };
 
